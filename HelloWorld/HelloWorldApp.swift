0d111c2e5cb5992ffdcf0f131242e975ba0f55b5
Jordan Christensen <jkchristensen36@gmail.com>
2023-06-04 13:02:52 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Jordan Christensen on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
